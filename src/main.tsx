e92b02db1348f6498a52e1433d8f406611db3883
syedhasnainpk <syedhasnainalipk@gmail.com>
2026-05-10 16:26:18 +0500

    Add Lenis smooth scroll

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Lenis from 'lenis'
+
+// Initialise Lenis smooth scroll
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothWheel: true,
+})
+
+function raf(time: number) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
